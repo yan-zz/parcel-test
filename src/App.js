993bbcb8eded9512c99ab7651a2930aa93ac1632
yan <yanzz@proton.me>
2022-04-30 13:08:06 -0400

    save parcel test project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Hello } from './Hello';
+import '../public/styles.css';
+
+export function App() {
+  return (
+    <div className="app">
+      <Hello />
+    </div>
+  );
+}
